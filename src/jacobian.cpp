0d01506e967418f367bc31d894bb91c2cf7e800d
nalchevanidze <d.nalchevanidze@gmail.com>
2019-11-27 16:11:23 +0100

    joints

--- src/jacobian.cpp
+++ src/jacobian.cpp
@@ -9,17 +9,34 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "jacobian_calculator");
     ros::AsyncSpinner spinner(1);
     spinner.start();
-    moveit::planning_interface::MoveGroupInterface move_group_left_leg("LeftLeg");
+
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+
+
+    moveit::planning_interface::MoveGroupInterface move_group_left_leg("LeftLeg");
+    
     robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+    
     ROS_ERROR("Model frame: %s", kinematic_model->getModelFrame().c_str());
+
     robot_state::RobotStatePtr kinematic_state = move_group_left_leg.getCurrentState();
+    
     const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("LeftLeg");
+
     Eigen::Vector3d reference_point_position(0.0, 0.0, 0.0);
     Eigen::MatrixXd jacobian;
+
+    const std::vector<robot_model::JointModel*>& joint_names = kinematic_model->getJointModels();
+
+    
+    for (std::size_t i = 0; i < joint_names.size(); ++i)
+    {
+        ROS_INFO("Joint: %s", joint_names[i]->getName().c_str());
+    }
+
     kinematic_state->getJacobian(joint_model_group,
                                kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
                                reference_point_position, jacobian);
+    
     ROS_INFO_STREAM("Jacobian: \n" << jacobian << "\n");
-
 }
