1fe88d9bca3646a1c4bdfff45afae83e2ee6d65a
nalchevanidze <d.nalchevanidze@gmail.com>
2020-01-08 18:13:24 +0100

    Implement dummy 'joint_state'-subscriber

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "RobotModelTools.h"
 #include "JacobianCalculator.h"
 #include "RobotMarkerPublisher.h"
+#include "sensor_msgs/JointState.h"
 
 
 using namespace std;
@@ -12,29 +13,44 @@ using namespace robot_state;
 using namespace ros;
 
 
+// Signature must be compatible to the respective topic
+void test_callback(const sensor_msgs::JointState::ConstPtr& msg)
+{
+    // TODO: Implement message handling
+    ROS_ERROR_STREAM("msg received");
+}
+
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "jacobian_calculator");
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
 
-    RobotModelLoader robotModelLoader("robot_description");
-    RobotModelPtr kinematicModel = robotModelLoader.getModel();
+    ros::NodeHandle n;
 
-    RobotModelTools robotModelTools;
-    // JacobianCalculator jacobianCalculator;
+    ros::Subscriber sub = n.subscribe("joint_states", 1000, test_callback);
+    ros::spin();
 
-    const vector<JointModelGroup*>& jointModelGroups = kinematicModel->getJointModelGroups();
-    vector<JointModelGroup*> chainedModelGroups = robotModelTools.getChainModelGroups(kinematicModel);
+    //ros::AsyncSpinner spinner(1);
+    //subscriber.startListening();
+    // spinner.start();
+
+    // RobotModelLoader robotModelLoader("robot_description");
+    // RobotModelPtr kinematicModel = robotModelLoader.getModel();
+
+    // RobotModelTools robotModelTools;
+    // // JacobianCalculator jacobianCalculator;
+
+    // const vector<JointModelGroup*>& jointModelGroups = kinematicModel->getJointModelGroups();
+    // vector<JointModelGroup*> chainedModelGroups = robotModelTools.getChainModelGroups(kinematicModel);
 
     
-    // calculate jacobians
-    // for(int i = 0; i < chainedModelGroups.size(); i ++ )
-    // {
-    //     JointModelGroup *currentJointGroup = chainedModelGroups[i];
-    //     jacobianCalculator.calculateJacobian(currentJointGroup, true);
-    // }
+    // // calculate jacobians
+    // // for(int i = 0; i < chainedModelGroups.size(); i ++ )
+    // // {
+    // //     JointModelGroup *currentJointGroup = chainedModelGroups[i];
+    // //     jacobianCalculator.calculateJacobian(currentJointGroup, true);
+    // // }
 
-    // publish markers
-    RobotMarkerPublisher publisher("vdm_markers_velocity", chainedModelGroups);
-    publisher.startPublishing();
+    // // publish markers
+    // RobotMarkerPublisher publisher("vdm_markers_velocity", chainedModelGroups);
+    // publisher.startPublishing();
 }
