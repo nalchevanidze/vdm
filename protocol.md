f32029d0196f10ea8aee91359894ec20a176ffb9
nalchevanidze <d.nalchevanidze@gmail.com>
2020-01-15 18:17:57 +0100

    Basic implementation of the subscription based marker publishing

--- protocol.md
+++ protocol.md
@@ -1,21 +1,27 @@
 # Protocol
 
-## week 1-2 
+## Week 1-2 
 
-we wrote python api 
-
-## week 3-4
-
-we discovered moveit
-
-decidet to migrate on c++
-
-## week 4-9
-
-- we implemented basic jacobian calculator
-- ponters
-- speed from moveit does not worked well, for us, (we dont know what it really does)
-- we moved to `Subscriber`
+- First demo-implementation of a moving robot in Python 
 
 
+## Week 3-4
 
+- Migration to C++ and moveit
+
+
+## Week 4-9
+
+- Implementation of a simple jacobian-calculator-class
+- Encountered problems while obtaining the velocities from moveit since we 
+  didn't know how to properly interprete the results
+- Migration to a `Subscriber`-based implementation
+
+
+## Week 10
+
+- Implemented functionality to listen to the 'joint_states' topic and publish markers as a reaction
+- Current implementation is ugly and needs to be put in a separate class
+- A simple static marker is published as a proof of concept
+- To-Do: Create marker based on the actual velocity as in the previous AbstractMarkerPublisher class
+  - Don't re-use AbstractMarkerPublisher and the related classes, it will probably be easier to re-write it
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "RobotMarkerPublisher.h"
 #include "sensor_msgs/JointState.h"
 
+#include <moveit/move_group_interface/move_group_interface.h>
 
 using namespace std;
 using namespace robot_model;
@@ -13,11 +14,63 @@ using namespace robot_state;
 using namespace ros;
 
 
+Publisher pub;
+int cnt = 1000;
+
+
+double calculateVelocity(time t1, double pos1, time t2, double pos2)
+{
+    return 1.0d;
+}
+
+
 // Signature must be compatible to the respective topic
 void test_callback(const sensor_msgs::JointState::ConstPtr& msg)
 {
-    // TODO: Implement message handling
-    ROS_ERROR_STREAM("msg received");
+    //time timestamp = msg->header.stamp;
+    //ROS_INFO_STREAM("Timestamp: " << timestamp);
+
+    V_string name = msg->name;
+    vector<double> velocity = msg->velocity;
+    vector<double> position = msg->position;
+
+    ROS_INFO_STREAM("Name size: " << name.size());
+    ROS_INFO_STREAM("Velocity size: " << velocity.size());
+    ROS_INFO_STREAM("Position size: " << position.size());
+
+    for (int i = 0; i < msg->position.size(); i++) {
+        ROS_INFO_STREAM("Current name: " << name[i]);
+        ROS_INFO_STREAM("Current pos: " << position[i]);
+        ROS_INFO_STREAM("Current position: " << position[i]);
+
+
+        // Marker only as a proof of concept - use outsourced method for creation instead
+
+        RobotModelLoader robotModelLoader("robot_description");
+        RobotModelPtr kinematicModel = robotModelLoader.getModel();
+        auto jointModel = kinematicModel->getJointModel(name[i]);
+        auto jointChildLinkModel = jointModel->getChildLinkModel();
+        string linkName = jointChildLinkModel->getName();
+
+        
+        visualization_msgs::MarkerArray markerArray;
+        visualization_msgs::Marker marker;
+        marker.header.frame_id = linkName;
+        marker.header.stamp = ros::Time();
+        marker.ns = "stats";
+        marker.id = ++cnt;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.scale.x = 0.03;
+        marker.scale.y = 0.03;
+        marker.scale.z = 0.03;
+        marker.color.a = 1.0;
+        marker.color.r = 0.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+        markerArray.markers.push_back(marker);
+
+        pub.publish(markerArray);
+    }
 }
 
 
@@ -26,8 +79,13 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle n;
 
+    pub = n.advertise<visualization_msgs::MarkerArray>("vdm_test_arrays", 0);
+
     ros::Subscriber sub = n.subscribe("joint_states", 1000, test_callback);
     ros::spin();
+
+
+    // TODO: Dead code, should be deleted in the future (AbstractMArkerPublisher too)!
 
     //ros::AsyncSpinner spinner(1);
     //subscriber.startListening();
@@ -36,13 +94,13 @@ int main(int argc, char** argv) {
     // RobotModelLoader robotModelLoader("robot_description");
     // RobotModelPtr kinematicModel = robotModelLoader.getModel();
 
-    RobotModelTools robotModelTools;
+    // RobotModelTools robotModelTools;
     // JacobianCalculator jacobianCalculator;
 
     // const vector<JointModelGroup*>& jointModelGroups = kinematicModel->getJointModelGroups();
     // vector<JointModelGroup*> chainedModelGroups = robotModelTools.getChainModelGroups(kinematicModel);
 
     // publish markers
-    RobotMarkerPublisher publisher("vdm_markers_velocity", chainedModelGroups);
-    publisher.startPublishing();
+    // RobotMarkerPublisher publisher("vdm_markers_velocity", chainedModelGroups);
+    // publisher.startPublishing();
 }
