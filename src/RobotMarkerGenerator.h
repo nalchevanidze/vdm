ad4d9ec55c38bce5164f01a0162e829e6d78ffaa
Hauke Sommerfeld <haukesomm@gmail.com>
2020-01-25 22:24:16 +0100

    Add rudimentary documentation to the header files

--- src/RobotMarkerGenerator.h
+++ src/RobotMarkerGenerator.h
@@ -8,20 +8,51 @@
 
 using namespace std;
 
+/// This class can be used to generate markers to be displayed in rviz.
 class RobotMarkerGenerator 
 {
     public:
+        /// Constructor
         RobotMarkerGenerator();
 
+        /// Creates a marker array that visualizes the given value in form
+        /// of an arrow and a text.
+        ///
+        /// The text simply displays the given value.
+        /// The arrow-shaped marker visualizes the value and varies in size
+        /// depending on the value.
+        ///
+        /// TODO: Convert 'value' to double and pass in additional prameters
+        /// for the name and unit.
         visualization_msgs::MarkerArray create(string name, string value);
 
+        /// Resets the internal id-counter.
+        ///
+        /// Internally each marker's id is created by using a counter that is
+        /// incremented each time 'create()' gets called. This way each marker
+        /// is assigned a unique id.
+        ///
+        /// This function resets that very counter which can be used to
+        /// intentionally re-assign ids.
+        /// This can be useful in case a bunch of markers is should be
+        /// updated periodically from within a for-loop instead of publishing a
+        /// new marker every time.
         void reset(); 
 
     private:
+        /// The counter used to determine a marker's id.
         int idCounter;
         
+        /// Creates a bare-bones marker attached to the given frame.
+        ///
+        /// This raw marker can then be further customized.
         visualization_msgs::Marker createMarker(string frameId);
+
+        /// Creates a text-label-marker attached to the given frame, displaying the given
+        /// message.
         visualization_msgs::Marker createMarkerLabel(string frameId, string marker_label);
+
+        /// Creates an arrow-shaped marker.
         visualization_msgs::Marker createMarkerArrow(string frameId);
 };
 
--- src/RobotModelTools.h
+++ src/RobotModelTools.h
@@ -8,9 +8,12 @@
 
 using namespace std;
 
+/// A tool class containing helpful robot-model-related functions.
 class RobotModelTools
 {
 public:
+    /// Returns a vector of all the joint's names contained in the given robot-
+    /// model.
     vector<string> getAllJointNames(robot_model::RobotModelPtr model);
 };
 
